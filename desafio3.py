eb5ad511814c018340c34c1b2843b2581fa70c8b
Pedro Braga <pedrobragalima@gmail.com>
2022-12-27 21:26:29 -0300

    questoes desafios

--- desafio3.py
+++ desafio3.py
@@ -3,7 +3,7 @@ lst_siglas    = ['AL', 'BA', 'CE', 'MA', 'PB', 'PE', 'PI', 'RN', 'SE']
 lst_populacao = [3365351, 14985284, 9240580, 7153262, 4059905, 9674793, 3289290, 3560903, 2338474]
 
 # O programa deve solicitar a sigla da uf e exibir as informações
-uf = lst_siglas.index(str.upper(input('informe a UF: ')))
-print(f'UF informada: {lst_siglas[uf]}')
 # caso a sigla informada exista na lista obedecendo o seguinte layout:
 # 'O estado NOME_ESTADO (SIGLA) possui (QUANTIDADE) habitantes'
+posicao_uf_sigla = lst_siglas.index(str.upper(input('informe a UF: ')))
+print(f'O estado {lst_siglas[posicao_uf_sigla]} possui {lst_populacao[posicao_uf_sigla]} habitantes')
