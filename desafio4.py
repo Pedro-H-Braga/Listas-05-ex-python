8c9cdc86108e76a8f5a91f491d038696522b942c
Pedro Braga <pedrobragalima@gmail.com>
2022-12-27 21:45:51 -0300

    questoes desafios FOR

--- desafio4.py
+++ desafio4.py
@@ -2,12 +2,16 @@ lst_uf        = ['Alagoas', 'Bahia', 'Ceará', 'Maranhão', 'Paraíba', 'Pernamb
 lst_siglas    = ['AL', 'BA', 'CE', 'MA', 'PB', 'PE', 'PI', 'RN', 'SE']
 lst_populacao = [3365351, 14985284, 9240580, 7153262, 4059905, 9674793, 3289290, 3560903, 2338474]
 
-# O programa deve solicitar a sigla da uf e exibir as informações
-# caso a sigla informada exista na lista obedecendo o seguinte layout:
-# 'O estado NOME_ESTADO (SIGLA) possui (QUANTIDADE) habitantes'
-posicao_uf_sigla = lst_siglas.index(str.upper(input('informe a UF: ')))
-print(f'O estado {lst_siglas[posicao_uf_sigla]} possui {lst_populacao[posicao_uf_sigla]} habitantes')
-
 # O programa deve solicitar a sigla da uf e excluir as informações
 # caso a sigla informada exista na lista, lembrar de excluir das 
 # demais listas.
+entrada = str.upper(input('informe a UF: '))
+posicao_uf_sigla = lst_siglas['RN']
+
+del lst_siglas[posicao_uf_sigla]
+del lst_populacao[posicao_uf_sigla]
+del lst_siglas[posicao_uf_sigla]
+
+print(f'{lst_siglas}')
+print(f'{lst_populacao}')
+print(f'{lst_siglas}')
