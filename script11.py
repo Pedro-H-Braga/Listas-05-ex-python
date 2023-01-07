08b9f049a81177ad2b3f7dd696ec715b94d14fe5
Pedro-H-Braga <pedrobragalima@gmail.com>
2023-01-07 19:17:08 -0300

    script 11

--- script11.py
+++ script11.py
@@ -9,15 +9,15 @@ pegar resto de a por b > dividir b pelo resto até ser igual a 0
 
 #a = int(input('Informe o primeiro numero: '))
 #b = int(input('Informe o segundo numero: '))
-a,b = 15,12
+a = 1135
+b = 120
 
-auxiliar = 0
-if auxiliar != 0:
-    for auxiliar in range(b):
-    #while b != 0:    
-        auxiliar = b
-        b = a % b
-        a = auxiliar
-
-
-print(f'O MDC pelo algoritmo de euclides é: {a}')
+auxiliar = 1
+for b in range(b>0):
+#while b != 0:  
+    # auxiliar  b
+    auxiliar = b
+    b = a % b
+    a = auxiliar
+    print(f'O MDC pelo algoritmo de euclides é: {a}')
+#else: 
