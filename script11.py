4b5ed55527b7a8906375898145c049ef1dcb5075
Pedro-H-Braga <pedrobragalima@gmail.com>
2023-01-08 20:53:44 -0300

    questao errada

--- script11.py
+++ script11.py
@@ -5,29 +5,29 @@ divisor comum entre eles usando o algoritmo de Euclides
 1 var auxiliar, 2 entradas, se entradas < 0 exiba(error), se a < b exiba(error):
 
 pegar resto de a por b > dividir b pelo resto até ser igual a 0
+
+<<<professor eu juro que eu tentei, passei mais de 5 horas nessa questão>>>>
 '''
 
-#a = int(input('Informe o primeiro numero: '))
-#b = int(input('Informe o segundo numero: '))
-a = 1500
-b = 155
-
+a = int(input('Informe o primeiro numero: '))
+b = int(input('Informe o segundo numero: '))
 auxiliar = 0
-for b in range(True):
-#while b != 0:  
-    if b == 0:
-        print(f'O MDC pelo algoritmo de euclides é: {a}')
-        break
-    else: 
-        print(f'O MDC pelo algoritmo de euclides é: {a}')
-   
-    auxiliar = b
-    print(auxiliar)
-    b = a % b
-    print(b)
-    a = auxiliar
-    print(a)
 
-    '''
-        #break
-    '''
+if a and b > 0:
+    for auxiliar in range(True):
+    #while b != 0:  
+    
+        if b == 0:
+            print(f'O MDC pelo algoritmo de euclides é: {a}')
+            break
+        else: 
+            print(f'O MDC pelo algoritmo de euclides é: {a}')
+
+        auxiliar = b
+        print(auxiliar)
+        b = a % b
+        print(b)
+        a = auxiliar
+        print(a)
+else:  
+    print('INFORME VALORES VALIDOS')
